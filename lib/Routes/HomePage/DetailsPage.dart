e1cc2496fb378f00280363797e38d4041a38cc46
kbrunet <killian.brunet@epitech.eu>
2020-09-24 14:28:14 +0200

    add

--- lib/Routes/HomePage/DetailsPage.dart
+++ lib/Routes/HomePage/DetailsPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:synthiaapp/Routes/HomePage/EditOrder.dart';
-import 'MultiSelect.dart';
 import '../../Services/Mailer.dart';
 import '../../auth.dart';
 

f6c6933c6758531524af6b2bf2ac6808c25a8d3c
Killian Brunet <killian.brunet@dataiku.com>
2021-06-19 18:09:10 +0200

    fix(dowloadpdf): now work on ios

--- lib/WebMobileFunction/mobile/save_and_launch_file.dart
+++ lib/WebMobileFunction/mobile/save_and_launch_file.dart
@@ -3,7 +3,7 @@ import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<void> saveAndLaunchFile(List<int> bytes, String fileName) async {
-  final path = (await getExternalStorageDirectory())!.path;
+  final path = (await getApplicationDocumentsDirectory()).path;
   final file = File('$path/$fileName');
   await file.writeAsBytes(bytes, flush: true);
   OpenFile.open('$path/$fileName');

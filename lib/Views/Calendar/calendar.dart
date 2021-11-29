7c8ea835051a2ac936547caf0757fa9b64445e52
GuillaumeHoub <guilllaume.houbion@epitech.eu>
2021-11-29 21:48:47 +0100

    [ADD] calendar.dart and add modules to pubspec.yaml

---
+++ lib/Views/Calendar/calendar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class Calendar extends StatefulWidget {
+  @override
+  State<Calendar> createState() => _Calendar();
+}
+
+class _Calendar extends State<Calendar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("Calendar"),
+        ),
+        body: SfCalendar(
+          view: CalendarView.month,
+          firstDayOfWeek: 1,
+          monthViewSettings: MonthViewSettings(showAgenda: true),
+          dataSource: _getCalendarDataSource(),
+        ));
+  }
+}
+
+_AppointmentDataSource _getCalendarDataSource() {
+  List<Appointment> appointments = <Appointment>[];
+  appointments.add(Appointment(
+    startTime: DateTime.now(),
+    endTime: DateTime.now().add(Duration(minutes: 10)),
+    subject: 'Meeting',
+    color: Colors.blue,
+    startTimeZone: '',
+    endTimeZone: '',
+  ));
+
+  return _AppointmentDataSource(appointments);
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
